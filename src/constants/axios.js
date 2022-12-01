46467053343fa3264ad06e8d58cc67d9e79edd5c
dhwani bhatt <bhattdhwani01@gmail.com>
2022-12-02 00:23:30 +0530

    changes after deployment

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const axiosBaseURL = axios.create({
 	// baseURL: "https://blogify-app-backend.herokuapp.com/api",
-	baseURL: "https://blogify-backend-71q1.vercel.app/",
+	// baseURL: "https://blogify-backend-71q1.vercel.app/",
+	baseURL: "https://blogify-backend-ruddy.vercel.app/api",
 });
